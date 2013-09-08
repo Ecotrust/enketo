6d9785cceb6540c49315d9d9589ebc729925ee0b
Matthew Perry <mperry@ecotrust.org>
2013-09-08 01:28:09 -0700

    initial tweak for formhub deploy

--- devinfo/database/instances.sql
+++ devinfo/database/instances.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.11.1deb1
+-- version 3.4.10.1deb1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 18, 2013 at 04:06 PM
--- Server version: 5.5.29
--- PHP Version: 5.4.6-1ubuntu1.1
+-- Generation Time: Oct 08, 2012 at 08:36 PM
+-- Server version: 5.5.24
+-- PHP Version: 5.3.10-1ubuntu3.4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -23,21 +23,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `properties`
+-- Table structure for table `instances`
 --
 
-CREATE TABLE IF NOT EXISTS `properties` (
-  `xsl_version` int(11) NOT NULL,
-  `form_xsl_hash` varchar(32) NOT NULL,
-  `model_xsl_hash` varchar(32) NOT NULL,
-  PRIMARY KEY (`xsl_version`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `properties`
---
-
--- INSERT INTO `properties` (`xsl_version`, `form_xsl_hash`, `model_xsl_hash`) VALUES (0, '', '');
+CREATE TABLE IF NOT EXISTS `instances` (
+  `instanceid` varchar(255) NOT NULL,
+  `subdomain` varchar(100) NOT NULL,
+  `return_url` varchar(255) NOT NULL,
+  `instance_xml` mediumtext,
+  `timestamp` bigint(20) NOT NULL,
+  UNIQUE KEY `idx_instanceid` (`instanceid`),
+  KEY `idx_subdomain` (`subdomain`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
